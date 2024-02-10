b31137754da8614813364ec726f5b81d59c9901b
bhand <bhand@MANOJ>
2024-02-10 22:08:39 +0545

    Add project files.

---
+++ signal r practise/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace signal_r_practise.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
